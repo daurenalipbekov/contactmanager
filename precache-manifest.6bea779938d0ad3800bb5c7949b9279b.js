98a6ca18226e5d8f16f30d3ab14548719c8ce9f1
alipbekovdauren <daurenh09@gmail.com>
2019-06-18 02:39:49 +0600

    Updates

---
+++ precache-manifest.6bea779938d0ad3800bb5c7949b9279b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648640ed8d71269be6715d23b9ef1f9b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "ebbc9b422e7b264252ee",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2abca56bfb207c74596f",
+    "url": "/contactmanager/static/js/2.26bc4f4d.chunk.js"
+  },
+  {
+    "revision": "ebbc9b422e7b264252ee",
+    "url": "/contactmanager/static/js/main.8f0fafa4.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
